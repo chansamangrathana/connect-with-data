3d951ea8ca46d6053fd500eae6161780457ffe89
chansamangrathana <chansamangrathana1607@gmail.com>
2024-04-08 22:47:33 -0700

    not yet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import model.Person;
 import repository.PersonRepository;
 import service.PersonService;
 import utils.TableUtils;
+
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,68 +14,64 @@ public class Main {
             new PersonService(new PersonRepository());
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int option;
+        List<String> mainMenu = new ArrayList<>(List.of("Add New Person", "Show person Information", "Search", "Exit"));
+        do {
+            TableUtils.renderMenu(mainMenu, "Person Management System");
+            System.out.print("Enter your option");
+            option = input.nextInt();
+            switch (option) {
+                case 2: {
+                    int showOption;
+                    List<String> showMenu = new ArrayList<>(List.of(
+                            "Show original",
+                            "Show Descending Order(ID) ",
+                            "Show Descending order(name)",
+                            "Exit"));
 
-        System.out.println("These are all the person in the repo: ");
-        personService.getAllPerson().forEach(
-                System.out::println
-        );
-        System.out.println("II. These are all the person in the repo: ");
-        personService.getAllPerson().forEach(
-                System.out::println
-        );
-          class main {
-            private static PersonService personService =
-                    new PersonService(new PersonRepository());
+                    do {
 
-            public static void main(String[] args) {
-                Scanner input = new Scanner(System.in);
-                int option;
+                        TableUtils.renderMenu(showMenu, "Show Peeson Information");
+                        System.out.println("Choose your option");
+                        showOption = input.nextInt();
+                        switch (showOption) {
+                            case 1:
+                                TableUtils.renderObjectToTable(personService.getAllPerson());
+                                break;
+                            case 2:
 
-                List<String> mainMenu = new ArrayList<>(List.of("Add New Person ", "Show Person Information", "Search", "Exit"));
-                do {
-                    TableUtils.renderMenu(mainMenu, "Person Management System");
-                    System.out.println("Enter your option : ");
-                    option = input.nextInt();
+                                TableUtils.renderObjectToTable(personService.getAllPerson()
+                                        .stream()
+                                        .sorted(
+                                                Comparator.comparingInt(Person::getId).reversed()
 
-                    switch (option) {
-                        case 2: {
-                            int showOption;
-                            List<String> showMenu = new ArrayList<>(List.of(
-                                    "Show Original Order",
-                                    "Show Descending Order (ID)",
-                                    "Show Descending order (name) ",
-                                    "Exit"));
-                            do {
+                                        )
+                                        .toList());
 
+                                break;
+                            case 3:
 
-                                TableUtils.renderMenu(showMenu, "Show Person Information");
-                                showOption = input.nextInt();
-                                switch (showOption) {
-                                    case 1:
-                                        TableUtils.renderObjectToTable(personService.getAllPerson());
-                                        break;
-                                    case 2:
-                                        break;
-                                    case 3:
-                                        System.out.println("Invalid option");
-                                }
-                            }while (showOption!=showMenu.size());
+                                break;
+                            default:
+                                System.out.println("Invalid");
+
                         }
+                    } while (showOption != showMenu.size());
+                }
+                break;
+                case 3:
+                    System.out.println("Search person information");
+                    break;
+                case 4:
+                    System.out.println("Exit from the program");
+                    break;
+                default:
+                    System.out.println("Invaild option");
 
-
-                        break;
-                        case 3:
-                            System.out.println("Search Person Information");
-                            break;
-                        case 4:
-                            System.out.println("Exit from the program!!! ");
-                            break;
-                        default:
-                            System.out.println("Invalid Option!!!!!! ");
-                            break;
-                    }
-                } while (option != 4);
             }
-        }
+
+
+        }while (option!=4);
     }
 }
